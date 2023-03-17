f7acef728f407ae7c12df9cf4ce816d0c9c22754
sung-min <yu9221@gmail.com>
2023-03-17 11:11:29 +0900

    update

--- packages/common/types.d.ts
+++ packages/common/types.d.ts
@@ -1,3 +1,20 @@
+/**
+ * 공통으로 사용할 declare 선언
+ *
+ * -
+ * package.json 에 타입정의 사용중이라는 것을 명시
+ * {
+ *  ...
+ *  "typings": "./types.d.ts",
+ *  ...
+ * }
+ *
+ * -
+ * 이 파일을 다른 곳(예를 들어 apps/mall)에서 사용하려면,
+ * apps/mall/typing.d.ts
+ * import '@makeapi/common';
+ * 코드를 넣어준다.
+ */
 import { Task } from 'redux-saga';
 import { GetServerSidePropsContext as _GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { TApiManager } from './apiManager';
@@ -20,7 +37,7 @@ declare module 'next-redux-wrapper' {
   type _GetServersidePropsCallback<
     S extends Store = ToolkitStore<TRootStateCommon>,
     P extends {} = any,
-    Strict extends boolean = false
+    Strict extends boolean = false,
   > = (conetxt: GetServerSidePropsContext<S>) => Strict extends true ? Promise<GetServerSidePropsResult<P>> : any;
 
   type GetServerSidePropsContext<S extends Store = ToolkitStore<TRootStateCommon>> = {
