2be565115ee8c9a07528b34e763b57ccb970f220
sung-min <yu9221@gmail.com>
2023-03-22 23:22:34 +0900

    update

---
+++ apps/community/babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['next/babel'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {},
+          extensions: ['.ts', '.tsx'],
+        },
+      ],
+    ],
+  };
+};
