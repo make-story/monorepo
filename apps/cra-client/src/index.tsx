4de7173545d2a9d909fe0109ca0f281e37a7f559
sung-min <yu9221@gmail.com>
2023-07-15 22:05:04 +0900

    update

--- apps/cra-client/src/index.tsx
+++ apps/cra-client/src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+//const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+/*root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+);*/
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
