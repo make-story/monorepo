c08df55c437492a17811fb4f14558db8940702d1
sung-min <yu9221@gmail.com>
2023-09-17 01:02:05 +0900

    update

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,7 +7,7 @@ module.exports = {
   arrowParens: 'avoid', // 화살표 함수 괄호
   bracketSpacing: true, // 대괄호 사이에 공백여부
   endOfLine: 'auto', // OS별 줄넘김 LF, CRLF
-  printWidth: 120, // 최대 줄 길이 (권장가이드 80)
+  printWidth: 80, // 최대 줄 길이 (권장가이드 80)
   semi: true, // 세미콜론 적용여부
   singleQuote: true, // 큰따옴표 대신 작은따옴표 사용여부
   jsxSingleQuote: true, // JSX에서 큰따옴표 대신 작은따옴표 사용여부
