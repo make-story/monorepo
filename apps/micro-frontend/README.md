e73648541248b6a78e9a8c8ea97ec5742e3a7001
sung-min <yu9221@gmail.com>
2023-10-19 17:41:17 +0900

    update

--- apps/micro-frontend/README.md
+++ apps/micro-frontend/README.md
@@ -3,3 +3,9 @@
 https://github.com/PacktPublishing/The-Art-of-Micro-Frontends
 
 https://thesametech.com/micro-frontend-migration-journey-part-1/
+
+## 웹컴포넌트 활용 방식
+
+웹컴포넌트: template tag(slot 포함) + shadow dom + custom element
+
+커스텀엘리먼트 사용 및 필요한 웹컴포넌트 파일(js 파일)동적 로드
---
+++ apps/vuejs-test/.eslintrc.js
@@ -0,0 +1,34 @@
+/**
+ * https://eslint.org/docs/latest/use/getting-started#configuration
+ * https://eslint.org/docs/latest/rules/
+ *
+ * ESLint 와 Prettier 충돌 해결
+ * eslint-config-prettier : eslint에서 prettier와 겹치는 포매팅룰을 삭제합니다.
+ * eslint-plugin-prettier : eslint에 prettier의 포매팅 기능을 추가합니다.
+ * eslint-config-pretteir로 eslint의 원래 포매팅 기능을 없애버리고, eslint-plugin-prettier로 prettier의 포매팅 기능을 사용합니다.
+ */
+
+module.exports = {
+  // ESLint 구성파일 탐색 범위
+  // default 는 true 인데, 이 값이 true 가 아니면, eslintrc 파일을 찾을 때,
+  // 해당 프로젝트 디렉토리 뿐 아니라, 내 PC의 root 파일 시스템 root 디렉토리까지 eslint 를 찾는다.
+  root: true,
+
+  // 프로젝트의 사용 환경을 설정한다.
+  // https://eslint.org/docs/latest/use/configure/language-options#specifying-environments
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+
+  // ESLint 사용을 위해 지원하려는 JavaScript 언어 옵션을 설정할 수 있다.
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
