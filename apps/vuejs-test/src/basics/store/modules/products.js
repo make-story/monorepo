ab3d0213b56e755d0ea710ba71768fd74c88a5a6
[상품상세개발팀] 유성민 <sungmin.yu01@lotte.net>
2023-11-08 18:17:04 +0900

    update

--- apps/vuejs-test/src/basics/store/modules/products.js
+++ apps/vuejs-test/src/basics/store/modules/products.js
@@ -1,37 +1,45 @@
-
 // 모든 Vuex 상태를 가집니다.
 const state = {
-	products: {}
+  products: {},
 };
 
 // 모든 Vue 게터를 가집니다.
+// getters
 const getters = {
-	products: state => state.products
+  products: (state, getters, rootState) => state.products,
 };
 
 // 모든 Vue 액션을 가집니다.
 // 비동기 코드를 위해 액션 객체를 사용합니다.
+// dispatch
 const actions = {
-	initStore: ({ commit/* context.commit */ }) => {
-		// 비동기 통신이 들어가는 곳
-		setTimeout(function() {
-			// 뮤테이션(mutations) 트리거 
-			commit('SET_STORE', {'test': 'YSM'});
-		}, 5000);
-	}
+  increment({ state, commit, rootState }) {
+    console.log('actions > increment', rootState);
+  },
+  initStore: ({ state, commit, rootState }) => {
+    // 비동기 통신이 들어가는 곳
+    setTimeout(function () {
+      // 뮤테이션(mutations) 트리거
+      commit('SET_STORE', { test: 'YSM' });
+    }, 5000);
+  },
 };
 
 // 모든 Vuex 뮤테이션을 가집니다.
+// commit
 const mutations = {
-	'SET_STORE'(state/* Vuex 상태값 */, products/* 사용자 데이터 */) {
-		state.products = products;
-	}
+  increment(state, payload) {
+    console.log('mutations > increment', payload);
+  },
+  SET_STORE(state /* Vuex 상태값 */, payload /* 사용자 데이터 */) {
+    state.products = payload;
+  },
 };
 
 export default {
-	namespaced: true,
-	state,
-	getters,
-	actions,
-	mutations,
+  namespaced: true, // true : 자동으로 등록된 모듈의 경로를 기반하여 네임스페이스 지정
+  state,
+  getters,
+  actions,
+  mutations,
 };
--- apps/vuejs-test/src/basics/store/store.js
+++ apps/vuejs-test/src/basics/store/store.js
@@ -9,7 +9,73 @@ Vue.use(Vuex);
 // store.commit('SET_STORE'); // mutations
 // store.dispatch('initStore'); // actions
 export const store = new Vuex.Store({
-    modules: {
-        products
-    }
+  modules: {
+    products,
+  },
 });
+
+/*
+// https://velog.io/@art11010/Vue-Vuex-%EB%AA%A8%EB%93%88
+const store = new Vuex.Store({
+  modules: {
+    account: {
+      namespaced: true,
+
+      // 모듈 자산
+      state: () => ({
+        // 모듈 상태는 이미 중첩되어 있고, 네임스페이스 옵션의 영향을 받지 않음
+        // ...
+      }),
+      getters: {
+        isAdmin() {
+          // -> getters['account/isAdmin']
+          // ...
+        },
+      },
+      actions: {
+        login() {
+          // -> dispatch('account/login')
+          // ...
+        },
+      },
+      mutations: {
+        login() {
+          // -> commit('account/login')
+          // ...
+        },
+      },
+
+      // 중첩 모듈
+      modules: {
+        // 부모 모듈로부터 네임스페이스를 상속받음
+        myPage: {
+          state: () => ({
+            // ...
+          }),
+          getters: {
+            profile() {
+              // -> getters['account/profile']
+              // ...
+            },
+          },
+        },
+
+        // 네임스페이스를 더 중첩
+        posts: {
+          namespaced: true,
+
+          state: () => ({
+            // ...
+          }),
+          getters: {
+            popular() {
+              // -> getters['account/posts/popular']
+              // ...
+            },
+          },
+        },
+      },
+    },
+  },
+});
+*/
