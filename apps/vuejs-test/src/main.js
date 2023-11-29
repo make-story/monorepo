aa081c5eda28fce8f2f99fefb26440bf5f1cd1f9
[상품상세개발팀] 유성민 <sungmin.yu01@lotte.net>
2023-11-29 15:24:15 +0900

    update

--- apps/vuejs-test/src/main.js
+++ apps/vuejs-test/src/main.js
@@ -5,9 +5,12 @@ import App from './App.vue';
 import { basicTest, vuexTest } from './basics';
 
 Vue.config.productionTip = false;
+const eventBus = (Vue.prototype.EventBus = new Vue());
 
 new Vue({
   render: h => h(App),
 }).$mount('#app');
 new Vue(basicTest);
 new Vue(vuexTest);
+
+export { eventBus };
