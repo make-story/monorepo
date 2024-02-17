72f497a361f2e2b97d0b74366550d38e41088676
sung-min <yu9221@gmail.com>
2024-02-17 21:58:58 +0900

    update

--- apps/vertical/src/common/utils/auth/token.ts
+++ apps/vertical/src/common/utils/auth/token.ts
@@ -1,5 +1,6 @@
-import type { Token } from 'next-auth/jwt';
 import { clone } from 'lodash';
+
+import type { Token } from 'next-auth/jwt';
 import type { OptionsType } from 'cookies-next/lib/types';
 
 import { initializeApollo } from '@/common/apollo/client/apolloClient';
@@ -8,8 +9,6 @@ import { REFRESH_TOKEN } from '@/common/queries/auth/auth.mutation';
 /**
  * 토큰 가져오기
  * 토큰 유효기간이 지낫을 경우 refresh token 요청 후 리턴
- *
- * @params token Muffin 토큰
  */
 export default async function getTokenApi(
   _token: Token,
